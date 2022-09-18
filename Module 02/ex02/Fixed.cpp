0a52f44cb8c8219807f27fe18c74f2d15966a69e
Soufiane Amajat <samajat@e1r5p8.1337.ma>
2022-09-18 21:21:46 +0100

    min/max

---
+++ Module 02/ex02/Fixed.cpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 12:50:49 by samajat           #+#    #+#             */
+/*   Updated: 2022/09/18 21:21:24 by samajat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Fixed.hpp"
+
+const int Fixed::fractBits = 8;
+
+Fixed::Fixed(void)
+{
+    raw = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+
+Fixed::Fixed   (const float f)
+{
+    std::cout << "Float constructor called" << std::endl;
+    raw = roundf(f * (1 << fractBits));
+}
+
+Fixed::Fixed   (const int d)
+{
+    std::cout << "Int constructor called" << std::endl;
+    raw = roundf(d << fractBits);
+}
+
+int     Fixed::getRawBits(void) const
+{
+    return (raw);
+}
+
+void    Fixed::setRawBits (int const raw)
+{
+    this->raw = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+    float f = (static_cast< float>(this->getRawBits()) / (1 << 8));
+    return (f);
+}
+
+int Fixed::toInt( void ) const
+{
+    int d = raw >> 8;
+    return (d);
+}
+
+Fixed& Fixed::operator=(const Fixed &fixed)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->raw = fixed.getRawBits();
+    
+    return (*this);
+}
+
+std::ostream& operator<<(std::ostream &COUT, const Fixed &fixed)
+{
+    float a = (static_cast< float>(fixed.getRawBits()) / (1 << 8));
+    COUT << a;
+    return (COUT);
+}
+
+static  Fixed& Fixed::min(const Fixed &f1, const Fixed &f2);
+{
+    if (f1.getRawBits() > f2.getRawBits())
+        return (f2);
+    return (f1)
+}
+
+static  Fixed& Fixed::max(const Fixed &f1, const Fixed &f2);
+{
+    if (f1.getRawBits() < f2.getRawBits())
+        return (f2);
+    return (f1)
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
---
+++ Module 02/ex02/Fixed.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 18:20:09 by samajat           #+#    #+#             */
+/*   Updated: 2022/09/18 21:20:58 by samajat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+//Headers
+#include <iostream>
+#include <cmath>
+//Class
+
+class Fixed
+{
+private:
+    static const int fractBits;
+    int              raw;
+
+public :
+
+    Fixed   (void);
+    Fixed   (const Fixed &copy);
+    Fixed   (const float f);
+    Fixed   (const int n);
+    Fixed   &operator =(const Fixed &fixed);
+    int     getRawBits(void) const;
+    void    setRawBits( int const raw );
+    float   toFloat( void ) const;
+    int     toInt( void ) const;
+    static  Fixed& min(const Fixed &f1, const Fixed &f2);
+    static  Fixed& max(const Fixed &f1, const Fixed &f2);
+    ~Fixed(void);
+    
+};
+//Adding an overloading function to take care of the case if the paramerter 2 is of Fixed type 
+std::ostream& operator <<(std::ostream &COUT, const Fixed &fixed);
+
+#endif
+
