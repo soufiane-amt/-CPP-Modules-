f262a10f3b7ca346465a6d6aa1ddebd2f952e5f1
Soufiane Amajat <samajat@e1r5p8.1337.ma>
2022-09-20 15:34:34 +0100

    testing

--- Module 02/ex03/bsp.cpp
+++ Module 02/ex03/bsp.cpp
@@ -6,37 +6,46 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 12:49:37 by samajat           #+#    #+#             */
-/*   Updated: 2022/09/20 14:35:50 by samajat          ###   ########.fr       */
+/*   Updated: 2022/09/20 15:06:40 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "Point.hpp"
 
-Fixed   getTwoPointsDistance(Point const p1, Point const p2)// add reference
+float   getTwoPointsDistance(Point const p1, Point const p2)// add reference
 {
-    Fixed dist(sqrt(pow(p2.getX() - pow(p1.getX())) + pow(p2.getY() - pow(p1.getY()))));
-    return (dist);
+    float   d = sqrt(pow(p2.getX().toFloat() - p1.getX().toFloat(), 2) + pow(p2.getY().toFloat() - p1.getY().toFloat(), 2));
+    // Fixed dist(d);
+    return (d);
 }
 
 
-Fixed   getSemiParm (Point const a, Point const b, Point const c)
+float   getSemiParm (Point const a, Point const b, Point const c)
 {
-    Fixed   two(2.0f);
-    Fixed   semiParameter ((getTwoPointsDistance(a, b) + getTwoPointsDistance(b, c) + getTwoPointsDistance(c, a)) / two);
-    return (semiParameter);
+    // Fixed   two(2.0f);
+    // Fixed   semiParameter ();
+    return ((getTwoPointsDistance(a, b) + getTwoPointsDistance(b, c) + getTwoPointsDistance(c, a)) / 2.0);
 }
 
 Fixed   getTriangleArea (Point const p1, Point const p2, Point const p3)
 {
-    Fixed   semiParam = getSemiParm(a, b, c);
-    Fixed Area(pow((semiParam * (semiParam - getTwoPointsDistance(a, b)) * (semiParam - getTwoPointsDistance(b, c))\
-    * (semiParam - getTwoPointsDistance(a, c))).toFloat(), semiParam));
+    float   semiParam = getSemiParm(p1, p2, p3);
+    float   area = pow(semiParam * (semiParam - getTwoPointsDistance(p1, p2)) * (semiParam - getTwoPointsDistance(p2, p3)) * (semiParam - getTwoPointsDistance(p1, p3)), semiParam);
+    Fixed Area(area);
 
     return (Area);    
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
+    (void)point;
+    std::cout << a.getX().toFloat() << std::endl;
+    //
+    std::cout << getTwoPointsDistance(a, b) << std::endl;
+    std::cout << getTwoPointsDistance(b, c) << std::endl;
+    std::cout << getTwoPointsDistance(a, c) << std::endl;
+    std::cout << getSemiParm(a, b, c) << std::endl;
     std::cout << getTriangleArea(a, b, c) << std::endl;
+    return true;
 }
