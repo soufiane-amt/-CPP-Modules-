753ab3b615daa186018482ddd3112fcbd03e2980
Soufiane Amajat <samajat@e1r5p8.1337.ma>
2022-09-22 14:07:07 +0100

    success

--- Module 02/ex03/bsp.cpp
+++ Module 02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 12:49:37 by samajat           #+#    #+#             */
-/*   Updated: 2022/09/21 21:48:13 by samajat          ###   ########.fr       */
+/*   Updated: 2022/09/22 14:04:13 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,75 @@
 #include "Point.hpp"
 #include <stdio.h>
 
+// float   getTwoPointsDistance(Point const p1, Point const p2)// add reference
+// {
+//     float   d = sqrt(pow(p2.getX().getRawBits() - p1.getX().getRawBits(), 2) + pow(p2.getY().getRawBits() - p1.getY().getRawBits(), 2));
+//     return (d);
+// }
+
+
+// float   getSemiParm (Point  a, Point  b, Point  c)
+// {
+//     float aa = (getTwoPointsDistance(a, b) + getTwoPointsDistance(b, c) + getTwoPointsDistance(c, a)) / 2;
+//     return aa;
+// }
+
+// float   getTriangleArea (Point const p1, Point const p2, Point const p3)
+// {    
+//     float semiParam = getSemiParm(p1, p2, p3);
+//     float f = sqrt((semiParam * (semiParam - getTwoPointsDistance(p1, p2)) * (semiParam - getTwoPointsDistance(p2, p3)) * (semiParam - getTwoPointsDistance(p1, p3))));
+
+//     return (f);
+// }
+
+// //check the border
+// bool bsp( Point const a, Point const b, Point const c, Point const point)
+// {
+//     printf("%0.20f\n", (getTriangleArea(a, b, c)));
+//     printf("%0.20f\n", getTriangleArea(point, a, b) + getTriangleArea(point, b, c) + getTriangleArea(point, c, a));
+    
+//     if (getTriangleArea(a, b, c)>=(int)((getTriangleArea(point, a, b) + getTriangleArea(point, b, c) + getTriangleArea(point, c, a))))
+//         std::cout << "yep" << std::endl;
+//     // std::cout << getTriangleArea(point, a, b) + getTriangleArea(point, b, c) << " -- "<< getTriangleArea(point, c, a) << std::endl;
+//     return true;
+// }
+
 float   getTwoPointsDistance(Point const p1, Point const p2)// add reference
 {
-    float   d = sqrt(pow(p2.getX().getRawBits() - p1.getX().getRawBits(), 2) + pow(p2.getY().getRawBits() - p1.getY().getRawBits(), 2));
+    float   d = sqrt(pow(p2.getX().toFloat() - p1.getX().toFloat(), 2) + pow(p2.getY().toFloat() - p1.getY().toFloat(), 2));
+    // Fixed dist(d);
     return (d);
 }
 
 
-float   getSemiParm (Point  a, Point  b, Point  c)
+float   getSemiParm (Point const a, Point const b, Point const c)
 {
-    float aa = (getTwoPointsDistance(a, b) + getTwoPointsDistance(b, c) + getTwoPointsDistance(c, a)) / 2;
-    return aa;
+    // Fixed   two(2.0f);
+    // Fixed   semiParameter ();
+    return ((getTwoPointsDistance(a, b) + getTwoPointsDistance(b, c) + getTwoPointsDistance(c, a)) / 2.0);
 }
 
 float   getTriangleArea (Point const p1, Point const p2, Point const p3)
-{    
-    float semiParam = getSemiParm(p1, p2, p3);
-    float f = sqrt((semiParam * (semiParam - getTwoPointsDistance(p1, p2)) * (semiParam - getTwoPointsDistance(p2, p3)) * (semiParam - getTwoPointsDistance(p1, p3))));
+{
+    float   semiParam = getSemiParm(p1, p2, p3);
+    float   area = sqrt(semiParam * (semiParam - getTwoPointsDistance(p1, p2)) * (semiParam - getTwoPointsDistance(p2, p3)) * (semiParam - getTwoPointsDistance(p1, p3)));
+    // Fixed Area(area);
 
-    return (roundf(f));
+    return (area);    
 }
 
-//check the border
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-    std::cout << (getTriangleArea(a, b, c)) << std::endl;
-    std::cout << getTriangleArea(point, a, b) << std::endl;
-    
-    if ((int)getTriangleArea(a, b, c)==(int)((getTriangleArea(point, a, b) + getTriangleArea(point, b, c) + getTriangleArea(point, c, a))))
-        std::cout << "yep" << std::endl;
-    // std::cout << getTriangleArea(point, a, b) << " -- "<< getTriangleArea(point, b, c) << " -- "<< getTriangleArea(point, c, a) << std::endl;
+    (void)point;
+    std::cout << a.getX().toFloat() << std::endl;
+    //
+    std::cout << getTwoPointsDistance(a, b) << std::endl;
+    std::cout << getTwoPointsDistance(b, c) << std::endl;
+    std::cout << getTwoPointsDistance(a, c) << std::endl;
+    std::cout << getSemiParm(a, b, c) << std::endl;
+    std::cout << getTriangleArea(a, b, c) << std::endl;
+    std::cout << (getTriangleArea(point, a, b) + getTriangleArea(point, b, c) + getTriangleArea(point, c, a)) << std::endl;
+    // if (getTriangleArea(a, b, c)>=((getTriangleArea(point, a, b) + getTriangleArea(point, b, c) + getTriangleArea(point, c, a))))
+    //     std::cout << "yep" << std::endl;
     return true;
 }
