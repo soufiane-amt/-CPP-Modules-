e14af55fc46e429eb607eec9b8055414751e77c1
Soufiane Amajat <samajat@e1r5p8.1337.ma>
2022-09-23 21:22:47 +0100

    take damage

--- Module 03/ex00/ClapTrap.cpp
+++ Module 03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 20:46:29 by samajat           #+#    #+#             */
-/*   Updated: 2022/09/23 21:07:47 by samajat          ###   ########.fr       */
+/*   Updated: 2022/09/23 21:22:27 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,28 @@ ClapTrap operator=(ClapTrap &copy)
     this->attackDamage = copy.attackDamage;
 }
 
+void ClapTrap::attack (const std::string& target)
+{
+    std::cout << "ClapTrap "<< this->name <<" attacks "<< target <<", causing "<< 1 <<" points of damage!" << std::endl;
+}
+
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if(amount <= this->energyPoints)
+    {
+        this->attackDamage += amount;
+        this->energyPoints -= amount;
+    }
+    else
+    {
+        this->attackDamage += this->energyPoints;
+        this->energyPoints  = 0;
+    }
+    std::cout << "ClapTrap "<< this->name <<" has been attacked "<< target <<", getting "<< amount <<" points of damage!" << std::endl;
+}
+
 ClapTrap::~ClapTrap (void)
 {
-    std::cout << this->name << "is destructed" << std::endl;     
+    std::cout << this->name << "is dead!" << std::endl;     
 }
