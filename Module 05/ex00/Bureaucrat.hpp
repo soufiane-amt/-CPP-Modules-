b4a1b4b0a6afc9152847364da4b4ff0f400c9d3a
Soufiane Amajat <samajat@e1r5p8.1337.ma>
2022-10-01 16:02:59 +0100

    starting up M5

---
+++ Module 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 15:39:02 by samajat           #+#    #+#             */
+/*   Updated: 2022/10/01 16:02:09 by samajat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT
+#define BUREAUCRAT
+
+//Header
+#include <iostream>
+#include <string>
+
+//Class
+class Bureaucrat
+{
+    public:
+    const std::string &getName(void);
+    int               &getGrade(void);
+    void              incrementGrade(void);
+    void              decrementGrade(void);
+
+    private:
+    const std::string name;
+    int              grade;
+};
+#endif
