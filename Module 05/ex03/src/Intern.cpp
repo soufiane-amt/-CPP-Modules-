0ed018d267ead1c4d69f289b31ca04e45d8faedc
Soufiane Amajat <samajat@e1r10p1.1337.ma>
2022-10-31 19:06:16 +0100

    saving

--- Module 05/ex03/src/Intern.cpp
+++ Module 05/ex03/src/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 13:41:10 by samajat           #+#    #+#             */
-/*   Updated: 2022/10/31 17:54:50 by samajat          ###   ########.fr       */
+/*   Updated: 2022/10/31 19:05:48 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,9 @@ Intern::Intern(void)
     forms[0] = &Intern::getPrsidentPard;
     forms[1] = &Intern::getRobotRequest;
     forms[2] = &Intern::getShrubberyCreat;
+    formNames[0] = "presidential";
+    formNames[1] = "robotomy request";
+    formNames[2] = "shrubbery request";
 }
 
 Intern::Intern(const Intern &obj)
@@ -48,25 +51,23 @@ int    Intern::formIsIndex(std::string n)
 
 Form*   Intern::makeForm(std::string formType, std::string target)
 {
-    Form *form = NULL;
-
-    switch (formIsIndex(formType))
-    {
-    case 0:
-        form = new RobotomyRequestForm(target);
-        break;
-    case 1:
-        form = new ShrubberyCreationForm(target);
-        break;
-    case 2:
-        form = new PresidentialPardonForm(target);
-        break;
-    default:
-        std::cout << "form name passed as parameter doesn’t exist. "<< std::endl;
-        exit(1);
-        break;
-    }
-    return (form);
+    // switch (formIsIndex(formType))
+    // {
+    // case 0:
+    //     return ((*this).*(forms[0]))(target);
+    //     break;
+    // case 1:
+    //     return ((*this).*(forms[1]))(target);
+    //     break;
+    // case 2:
+    //     return ((*this).*(forms[2]))(target);
+    //     break;
+    // default:
+    //     std::cout << "form name passed as parameter doesn’t exist. "<< std::endl;
+    //     break;
+    // }
+    (void)formType  ;(void)target;
+    return (NULL);
 }
 
 Form    *getPrsidentPard(std::string target)
