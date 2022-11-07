01e52e237703d1cf89e5f743740e6cb819715adb
Soufiane Amajat <samajat@e1r6p10.1337.ma>
2022-11-08 00:14:01 +0100

    mzn

--- Module 08/ex00/easyfind.hpp
+++ Module 08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 20:55:37 by samajat           #+#    #+#             */
-/*   Updated: 2022/11/07 23:30:10 by samajat          ###   ########.fr       */
+/*   Updated: 2022/11/08 00:13:28 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,25 @@
 
 #include<iostream>
 #include <iterator>
-// #include <algorithm>
+#include <algorithm>
 #include <vector>
+#include <array>
+#include <list>
 
 template <typename T>
 void easyfind(T &container, int i)
 {
     typename T::iterator iter;
-    for ( iter = container.begin(); iter != container.end(); iter++)
+    int index = 0;
+    for (  iter = container.begin(); iter != container.end(); iter++)
     {
+        index++;
         if (i == *iter)
         {
-            std::cout << "The element exists in index: "  << iter - container.begin() << std::endl;
+            std::cout << "The element exists in index: "  << index << std::endl;
             return;
         }
     }
-    std::cout << "The element to search is missing " << std::endl;
+    throw std::exception();
 }
 #endif
--- Module 08/ex00/main.cpp
+++ Module 08/ex00/main.cpp
@@ -6,30 +6,36 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 21:02:58 by samajat           #+#    #+#             */
-/*   Updated: 2022/11/07 22:49:55 by samajat          ###   ########.fr       */
+/*   Updated: 2022/11/08 00:05:06 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+#define TOSEARCH 5
 int main()
 {
+    {
     std::vector <int> arr;
     arr.push_back(12);
     arr.push_back(5);
     arr.push_back(3);
     arr.push_back(4);
-    arr.push_back(5);
-    easyfind(arr, 5);
-    // std::vector<int>::iterator it;
-    // for (it = arr.begin(); it != arr.end(); ++it)
-    // {
-    //     std::cout << *it << std::endl;
-    //     if (5 == *it)
-    //     {
-    //         std::cout << "The element exists in index: "  << it - arr.begin() << std::endl;
-    //         return 1;
-    //     }
-    // }
+    arr.push_back(55);
+    easyfind(arr, TOSEARCH);
+    }
+    {
+    std::array <int, 5> arr = {12, 5, 3, 4, 55};
+    easyfind(arr, TOSEARCH);
+    }
+    {
+        std::list<int>l;
+        for (int i = 0; i < 10; ++i) 
+        {
+            l.push_back(i * 2);
+            l.push_front(i * 3);
+        }
+        easyfind(l, TOSEARCH);
+    }
     return 0;
 }
