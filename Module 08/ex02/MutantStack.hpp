81c1f71d6ad482ef744389a2e31ea21f16568bc1
Soufiane Amajat <samajat@e1r13p4.1337.ma>
2022-11-09 17:26:38 +0100

    begin /end ..

--- Module 08/ex02/MutantStack.hpp
+++ Module 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 21:30:03 by samajat           #+#    #+#             */
-/*   Updated: 2022/11/09 01:57:05 by samajat          ###   ########.fr       */
+/*   Updated: 2022/11/09 17:02:23 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,11 @@
 template <class T, class containerType=std::deque<T> >
 class MutantStack
 {
-    typename containerType::size_type variable_x;
+    // typename containerType::size_type variable_x;
     containerType container;
     public:
-    typename containerType::size_type;
+    // typename containerType::size_type;
+
     //Conanical Form
     explicit MutantStack(const containerType& ctnr = containerType());
     MutantStack(const MutantStack& copy);
@@ -35,12 +36,103 @@ class MutantStack
     bool empty() const;
     T& top();
     const T& top() const; 
-    variable_x size() const;
+    typename containerType::size_type size() const;
     void push (const T& val);
+    
     // void emplace (typename Args&&... args);
     void pop();
-    // void swap (MutantStack& x) noexcept;
+    void swap (MutantStack& x);
+    
+    //Iterator
+    T* begin();
+    T* end();
+    class iterator
+    {
+        T *ptr;
+        public:
+        iterator();
+        iterator(const iterator& copy);
+        iterator& operator=(const iterator& copy);
+        ~iterator();
+        //
+    };
 };
 
+template <class T, class containerType>
+MutantStack<T, containerType>::MutantStack(const containerType& s)
+{
+    (void)s;
+}
+
+template <class T, class containerType>
+MutantStack<T, containerType>::MutantStack(const MutantStack& copy)
+{
+    (void)copy;
+}
+
+template <class T, class containerType>
+MutantStack<T, containerType>& MutantStack<T, containerType>::operator=(const MutantStack& copy)
+{
+    (void)copy;
+
+}
+
+template <class T, class containerType>
+MutantStack<T, containerType>::~MutantStack()
+{
+    
+}
+
+
+
+
+template <class T, class containerType>
+bool MutantStack<T, containerType>::empty() const
+{
+    return (container.empty());
+}
+
+
+template <class T, class containerType>
+T& MutantStack<T, containerType>::top()
+{
+    return (container.back());
+}
+
+template <class T, class containerType>
+const T& MutantStack<T, containerType>::top() const
+{
+    return (container.back());
+}
+
+template <class T,  class containerType>
+typename containerType::size_type MutantStack<T, containerType>::size() const
+{
+    return(container.size());
+}
+
+template <class T, class containerType>
+void MutantStack<T, containerType>::push (const T& val)
+{
+    container.push_back(val);
+}
+
+template <class T, class containerType>
+void MutantStack<T, containerType>::pop()
+{
+    container.pop_back();
+}
+
+template <class T, class containerType>
+void MutantStack<T, containerType>::swap (MutantStack& x)
+{
+    container.swap(x.container);
+}
+
+// template <class T, class containerType>
+//  void MutantStack<T, containerType>::emplace (typename Args &&... args)
+// {
+    
+// }
 
 #endif
--- Module 08/ex02/main.cpp
+++ Module 08/ex02/main.cpp
@@ -6,11 +6,35 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 21:30:02 by samajat           #+#    #+#             */
-/*   Updated: 2022/11/09 01:38:38 by samajat          ###   ########.fr       */
+/*   Updated: 2022/11/09 17:26:26 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+#include <vector>
+#include <ostream>
+#include <functional>
+class x
+{
+    int a;
+    public:
+    x(int a){this->a=a;}
+    int getX()const{return a;}
+};
+std::ostream& operator<<(std::ostream &COUT, const x& xc)
+{
+    COUT << xc.getX();
+    return (COUT);
+}
+void    func()
+{
+    std::cout << "hello world\n";
+}
+void    func1()
+{
+    std::cout << "helhilo\n";
+}
 
 int main()
 {
@@ -20,9 +44,9 @@ int main()
     stack.push(24);
     stack.push(25);
        
-    stack.pop();
-    stack.pop();
-   
+    // stack.pop();
+    // stack.pop();
+    std::cout << stack.size() << std::endl;
     while (!stack.empty()) {
         std::cout << stack.top() <<" ";
         stack.pop();
