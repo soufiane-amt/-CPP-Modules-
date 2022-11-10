e818539a03d1f22dfb651fa13f8d4c238dc750f5
Soufiane Amajat <samajat@e2r10p4.1337.ma>
2022-11-10 17:19:39 +0100

    salit

--- Module 08/ex02/MutantStack.hpp
+++ Module 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 21:30:03 by samajat           #+#    #+#             */
-/*   Updated: 2022/11/09 21:08:37 by samajat          ###   ########.fr       */
+/*   Updated: 2022/11/10 17:19:05 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,206 +16,67 @@
 #include <iostream>
 #include <deque>
 #include <stdexcept>
+#include <stack>
 
-
-template <class T, class containerType=std::deque<T> >
-class MutantStack
+template <class T , class containerType=std::deque<T> >
+class MutantStack : public std::stack<T>
 {
-    containerType container;
     public:
-
     //Conanical Form
     explicit MutantStack(const containerType& ctnr = containerType());
     MutantStack(const MutantStack& copy);
     MutantStack& operator=(const MutantStack& copy);
     ~MutantStack();
 
-    //Data manipulation functions
-    bool empty() const;
-    T& top();
-    const T& top() const; 
-    typename containerType::size_type size() const;
-    void push (const T& val);
-    
-    // void emplace (typename Args&&... args);
-    void pop();
-    void swap (MutantStack& x);
-    
-    //Iterator
-    template <class container=containerType>
-    class iterator
-    {
-        private:
-        container *ptr;
-        
-        public:
-        iterator(container* pointer=0);
-        iterator(const iterator& copy);
-        iterator& operator=(const iterator& copy);
-        iterator& operator++();
-        iterator& operator++(int);
-        iterator& operator--();
-        iterator& operator--(int);
-        ~iterator();
-    };
-    iterator begin();
-    iterator end  ();
+    typedef typename containerType::iterator iterator;
+    iterator  begin();
+    iterator  end  ();
 };
 
 
 //Conanical Form
 
-template <class T, class containerType>
+template <class T , class containerType>
 MutantStack<T, containerType>::MutantStack(const containerType& s)
 {
-    container = s;
+    this->c = s;
 }
 
-template <class T, class containerType>
+template <class T , class containerType>
 MutantStack<T, containerType>::MutantStack(const MutantStack& copy)
 {
     *this = copy;
 }
 
-template <class T, class containerType>
+template <class T , class containerType>
 MutantStack<T, containerType>& MutantStack<T, containerType>::operator=(const MutantStack& copy)
 {
-   for (int i =  container.size() - 1; i >= 0; i--)
+   for (int i =  this->c.size() - 1; i >= 0; i--)
         this->push(copy.container[i]);
     return (*this);
 }
 
-template <class T, class containerType>
+template <class T , class containerType>
 MutantStack<T, containerType>::~MutantStack()
 {
 }
 
 
-
-
-template <class T, class containerType>
-bool MutantStack<T, containerType>::empty() const
-{
-    return (container.empty());
-}
-
-
-template <class T, class containerType>
-T& MutantStack<T, containerType>::top()
-{
-    return (container.back());
-}
-
-template <class T, class containerType>
-const T& MutantStack<T, containerType>::top() const
-{
-    return (container.back());
-}
-
-template <class T,  class containerType>
-typename containerType::size_type MutantStack<T, containerType>::size() const
-{
-    return(container.size());
-}
-
-template <class T, class containerType>
-void MutantStack<T, containerType>::push (const T& val)
-{
-    container.push_back(val);
-}
-
-template <class T, class containerType>
-void MutantStack<T, containerType>::pop()
-{
-    container.pop_back();
-}
-
-template <class T, class containerType>
-void MutantStack<T, containerType>::swap (MutantStack& x)
-{
-    container.swap(x.container);
-}
-
-
-// template <class T, class containerType>
-//  void MutantStack<T, containerType>::emplace (typename Args &&... args)
-// {
+// Iterator
     
-// }
-
-//Iterator
-    
-template <class T, class containerType>
-typename MutantStack<T, containerType>::iterator MutantStack<T, containerType>::begin()
+template <class T , class containerType>
+typename containerType::iterator MutantStack<T, containerType>::begin()
 {
-    return (iterator(&container));
+    return (this->c.begin());
 }
 
-template <class T, class containerType>
-typename MutantStack<T, containerType>::iterator MutantStack<T, containerType>::end ()
+template <class T , class containerType>
+typename containerType::iterator MutantStack<T, containerType>::end ()
 {
-    return (iterator(&container + container.size() - 1));
+    return (this->c.end());
 }
 
 
-template <class T, class containerType>
-template <class container>
-typename MutantStack<T, containerType>::iterator::iterator(container* pointer):ptr(pointer)
-{
-    
-}
-
-template <class T, class containerType>
-template <class container>
-typename MutantStack<T, containerType>::iterator::iterator(const iterator& copy):ptr(copy.ptr)
-{
-}
-
-template <class T, class containerType>
-template <class container>
-typename MutantStack<T, containerType>::iterator& MutantStack<T, containerType>::iterator::operator=(const iterator& copy)
-{
-    this->ptr = copy.ptr;
-}
-
-template <class T, class containerType>
-template <class container>
-typename MutantStack<T, containerType>::iterator::~iterator()
-{
-}
-
-//
-
-template <class T, class containerType>
-template <class container>
-typename MutantStack<T, containerType>::iterator& MutantStack<T, containerType>::iterator::operator++(int)
-{
-    ptr++;
-    return (ptr - 1);
-}
-
-template <class T, class containerType>
-typename MutantStack<T, containerType>::iterator& MutantStack<T, containerType>::iterator::operator++()
-{
-    ptr++;
-    return (ptr);
-}
-
-template <class T, class containerType>
-template <class container>
-MutantStack<T, containerType>::iterator& MutantStack<T, containerType>::iterator::operator--(int)
-{
-    ptr--;
-    return (ptr + 1);
-}
-
-template <class T, class containerType>
-template <class container>
-typename MutantStack<T, containerType>::iterator& MutantStack<T, containerType>::iterator::operator--()
-{
-    ptr--;
-    return (ptr);
-}
 
 
 #endif
--- Module 08/ex02/main.cpp
+++ Module 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 21:30:02 by samajat           #+#    #+#             */
-/*   Updated: 2022/11/09 20:46:38 by samajat          ###   ########.fr       */
+/*   Updated: 2022/11/10 17:19:22 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,26 @@
 int main()
 {
     MutantStack<int> mstack;
-    // mstack.push(5);
-    // mstack.push(17);
-    // std::cout << mstack.top() << std::endl;
-    // mstack.pop();
-    // std::cout << mstack.size() << std::endl;
-    // mstack.push(3);
-    // mstack.push(5);
-    // mstack.push(737);
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    std::cout << mstack.size() << std::endl;
     // //[...]
-    // mstack.push(0);
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    // ++it;
-    // --it;
-    // while (it != ite)
-    // {
-    // std::cout << *it << std::endl;
-    // ++it;
-    // }
-    // std::stack<int> s(mstack);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
     return 0;
 }
